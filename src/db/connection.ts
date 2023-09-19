513c0a578feb0118463985cff00572fc6a5fde33
Hariom051 <hariom.rdec051@gmail.com>
2023-09-19 19:36:17 +0530

    gd

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,6 @@
 
 import mongoose from "mongoose";
-const promise= mongoose.connect(process.env.MONGOURL as string);
+const promise= mongoose.connect(`mongodb+srv://${process.env.NAME}:${process.env.PASS}@cluster0.wygtiqc.mongodb.net/userdb?retryWrites=true&w=majority`);
 promise.then(()=>{
   console.log("mongodb connection created")
 }).catch((err?:Error)=>{
